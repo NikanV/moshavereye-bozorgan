fddc4584e2ed02e5522877e1e1cd2456a888248d
NikanV <nikanvsiuni@gmail.com>
2021-12-17 01:09:23 +0330

    nikan

--- index.php
+++ index.php
@@ -1,8 +1,35 @@
 <?php
-$question = 'این یک پرسش نمونه است';
-$msg = 'این یک پاسخ نمونه است';
-$en_name = 'hafez';
-$fa_name = 'حافظ';
+//request part//
+$question = NULL;
+$person = NULL;
+//if he presses the button the values will change
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $question = $_POST["question"];
+    $person = $_POST["person"];
+}
+//people part//
+$people_info = file_get_contents("people.json");
+//converts json file details into an array
+$decoded_people_info = json_decode($people_info, true);
+if($person == NULL){
+    $en_name = array_rand($decoded_people_info);
+}
+else{
+    $en_name = $person;
+}
+$fa_name = $decoded_people_info[$en_name];
+//messages part//
+$empty_msg = "سوال خود را بپرس!";
+$messages = file_get_contents("messages.txt");
+//using explode to extract each line
+$each_msg = explode("\n", $messages);
+//hash will convert two variables into num and substr takes a part of it 
+//and hexdec converts it into decimal
+$msg = $each_msg[hexdec(substr(hash('gost-crypto',$question.$person),0,15))%count($each_msg)];
+//preg match will find whether the value is used in string or not (we should put \ for escape key)
+if(!preg_match("/آیا/i", $question) || (!preg_match("/\?/i", $question) && !preg_match("/\؟/i", $question))){
+    $msg = 'سوال درستی پرسیده نشده';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,12 +42,21 @@ $fa_name = 'حافظ';
 <p id="copyright">تهیه شده برای درس کارگاه کامپیوتر،دانشکده کامییوتر، دانشگاه صنعتی شریف</p>
 <div id="wrapper">
     <div id="title">
+        <?php if($question != NULL) { ?>
         <span id="label">پرسش:</span>
         <span id="question"><?php echo $question ?></span>
+    <?php } ?>
     </div>
     <div id="container">
         <div id="message">
-            <p><?php echo $msg ?></p>
+            <p><?php 
+            if($question == NULL){
+                echo $empty_msg;
+            }
+            else{
+                echo $msg;
+            } 
+            ?></p>
         </div>
         <div id="person">
             <div id="person">
@@ -34,15 +70,16 @@ $fa_name = 'حافظ';
             سوال
             <input type="text" name="question" value="<?php echo $question ?>" maxlength="150" placeholder="..."/>
             را از
-            <select name="person">
-                <?php
-                /*
-                 * Loop over people data and
-                 * enter data inside `option` tag.
-                 * E.g., <option value="hafez">حافظ</option>
-                 */
-                ?>
-            </select>
+            <select name="person"><?php
+            foreach($decoded_people_info as $key => $value) {
+                if($key == $en_name) {?>
+			    <option value="<?php echo $key; ?>" selected><?php echo $value ?></option><?php 
+                } 
+                else{
+                    ?><option value="<?php echo $key; ?>"><?php echo $value ?></option><?php 
+                }
+            }
+            ?></select>
             <input type="submit" value="بپرس"/>
         </form>
     </div>
